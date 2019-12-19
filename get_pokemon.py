0fa06111c94046e55790d62a934f4bf360b33233
Alex Gonzales <agonzales5071@gmail.com>
2019-12-19 01:03:28 -0600

    added name and numbers scrape
    name
    galnum
    natnum

--- get_pokemon.py
+++ get_pokemon.py
@@ -12,12 +12,26 @@ def get_poke_url():
 		links.append(SEREBII_URL + line.strip())
 	return links
 
+def scrape_page(url):
+	pokemon_data = {}
+	request = requests.get(url)
+	soup = BeautifulSoup(request.text, 'html.parser')
+	dextab = soup.find("table", attrs={"class": "dextab"})
+	dextables = soup.find_all("table", attrs={"class": "dextable"})
 
+	
+	pokemon_data["name"] = soup.find("h1").text.split(" ")[1]
+	pokemon_data["nat_num"] = int(dextables[1].find_all("tr")[1].find_all("td", attrs={"class": "fooinfo"})[2].find_all("td")[1].text.replace("#", ""))
+	pokemon_data["gal_num"] = int(dextables[1].find_all("tr")[1].find_all("td", attrs={"class": "fooinfo"})[2].find_all("td")[3].text.replace("#", ""))
+	
+	print(pokemon_data)
 
 
 
 def main():
-	print(get_poke_url())
+
+	links = get_poke_url()
+	scrape_page(links[0])
 
 
 main()
